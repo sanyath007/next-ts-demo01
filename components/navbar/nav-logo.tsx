288e8173f6ea53407bbdcde25979c76682351cb9
sanyath007 <sanyath007@gmail.com>
2023-04-11 21:10:00 +0700

    Reflactor Navbar component by splitting BottomMenu to itself component and modify NavLogo by temporiry removing menu-option section

--- components/navbar/nav-logo.tsx
+++ components/navbar/nav-logo.tsx
@@ -2,15 +2,15 @@
 export default function NavLogo() {
     return (
         <div className="container mx-auto">
-            <div className="relative flex justify-between items-center pt-2">
+            <div className="relative flex justify-between items-center pt-1">
                 <a href="#" className="logo flex items-center">
                     <img src="https://upload.wikimedia.org/wikipedia/commons/f/f9/%E0%B8%95%E0%B8%A3%E0%B8%B2%E0%B8%81%E0%B8%A3%E0%B8%B0%E0%B8%97%E0%B8%A3%E0%B8%A7%E0%B8%87%E0%B8%AA%E0%B8%B2%E0%B8%98%E0%B8%B2%E0%B8%A3%E0%B8%93%E0%B8%AA%E0%B8%B8%E0%B8%82%E0%B9%83%E0%B8%AB%E0%B8%A1%E0%B9%88.png" alt="logo" />
-                    <div className="logo-text flex flex-col justify-center pt-2">
+                    <div className="logo-text flex flex-col justify-center">
                         <h1>ศูนย์สุขภาพจิตที่ 9</h1>
                         <p>Mental Health Center 9</p>
                     </div>
                 </a>
-                <div className="menu-options flex items-center gap-5">
+                {/* <div className="menu-options flex items-center gap-5">
                     <div className="options flex items-center space-x-1">
                         <p>ขนาดตัวอักษร</p>
                         <a href="#" className="hover:bg-gray-300 bg-gray-200 px-2 text-sm rounded-md">ก</a>
@@ -31,7 +31,7 @@ export default function NavLogo() {
                             </select>
                         </div>
                     </div>
-                </div>
+                </div> */}
 
                 {/* ================================ nav mobile ================================ */}
                 <div className="mobile-menu">
