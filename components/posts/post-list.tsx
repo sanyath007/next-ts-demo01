e4098e4b0c41e1d177dfa86e50842c88fb5c047c
sanyath007 <sanyath007@gmail.com>
2023-04-12 15:46:08 +0700

    Modify calendar section by adding heading with border

--- components/posts/post-list.tsx
+++ components/posts/post-list.tsx
@@ -22,11 +22,11 @@ export default function PostList () {
 
                 <div className="lg:w-1/4 xl:w-2/6 flex flex-col text-center pl-4 pb-4 mb-2 space-y-2 sm:hidden lg:block">
                     {/* Director */}
-                    <div className="border rounded-md p-2 mb-4 flex flex-col items-center justify-center space-y-2">
+                    <div className="border rounded-md p-2 mb-5 flex flex-col items-center justify-center space-y-2">
                         <img
                             src="https://www.mhc9dmh.com/data/photo/807_%e0%b8%9c%e0%b8%ad%20%e0%b8%95%e0%b8%b8%e0%b9%88%e0%b8%a1.jpg"
                             alt="director-image"
-                            className="w-[180px] mt-4"
+                            className="w-[200px] mt-4 mb-2"
                         />
                         <h2 className="text-lg xl:text-xl">นางสาวศิริลักษณ์ แก้วเกียรติพงษ์</h2>
                         <h4 className="text-md xl:text-lg">ผู้อำนวยการศูนย์สุขภาพจิตที่ 9</h4>
@@ -34,6 +34,10 @@ export default function PostList () {
 
                     {/* Calendar */}
                     <div className="border rounded-md p-2 mb-4 h-auto">
+                        <h1 className="title text-xl">ปฏิทินกิจกรรม</h1>
+
+                        <hr className="mt-2" />
+
                         <CalendarWrapper fullscreen={false} />
                     </div>
                 </div>
