309cf7b46734ae7f9de35a817ad8ceeb8d8e0dcf
sanyath007 <sanyath007@gmail.com>
2023-04-19 22:35:45 +0700

    Move all components of navbar to from NavLogo to Navbar

--- components/navbar/nav-logo.tsx
+++ components/navbar/nav-logo.tsx
@@ -1,59 +1,12 @@
-import NavMenu from "./nav-menu";
 
 export default function NavLogo() {
     return (
-        <div className="container mx-auto">
-            <div className="relative flex items-end justify-between pt-1">
-                <a href="#" className="logo flex items-center">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/f/f9/%E0%B8%95%E0%B8%A3%E0%B8%B2%E0%B8%81%E0%B8%A3%E0%B8%B0%E0%B8%97%E0%B8%A3%E0%B8%A7%E0%B8%87%E0%B8%AA%E0%B8%B2%E0%B8%98%E0%B8%B2%E0%B8%A3%E0%B8%93%E0%B8%AA%E0%B8%B8%E0%B8%82%E0%B9%83%E0%B8%AB%E0%B8%A1%E0%B9%88.png" alt="logo" />
-                    <div className="logo-text flex flex-col justify-center">
-                        <h1>ศูนย์สุขภาพจิตที่ 9</h1>
-                        <p>Mental Health Center 9</p>
-                    </div>
-                </a>
-
-                {/* ================================ Nav menus ================================ */}
-                <NavMenu />
-                {/* ================================ End nav menus ================================ */}
-
-                {/* ================================ Menu Options ================================ */}
-                {/* <div className="menu-options flex items-center gap-5">
-                    <div className="options flex items-center space-x-1">
-                        <p>ขนาดตัวอักษร</p>
-                        <a href="#" className="hover:bg-gray-300 bg-gray-200 px-2 text-sm rounded-md">ก</a>
-                        <a href="#" className="hover:bg-gray-300 bg-gray-200 px-2 text-lg rounded-md">ก</a>
-                        <a href="#" className="hover:bg-gray-300 bg-gray-200 px-2 text-2xl rounded-md">ก</a>
-                    </div>
-                    <div className="options flex items-center space-x-1">
-                        <p>ความตัดกันของสี</p>
-                        <a href="#" className="hover:bg-gray-300 bg-gray-200 text-sm font-bold px-2 py-1 rounded-md">C</a>
-                        <a href="#" className="hover:bg-gray-500 bg-gray-600 text-sm font-bold text-white hover:text-gray-100 px-2 py-1 rounded-md">C</a>
-                        <a href="#" className="hover:bg-gray-700 bg-black text-sm font-bold text-yellow-300 px-2 py-1 rounded-md">C</a>
-                    </div>
-                    <div className="options flex items-center space-x-1">
-                        <div className="border px-2 rounded-lg">
-                            <select className="outline-none w-full">
-                                <option value="">ภาษาไทย</option>
-                                <option value="">English</option>
-                            </select>
-                        </div>
-                    </div>
-                </div> */}
-                {/* ================================ End menu Options ================================ */}
-
-                {/* ================================ Nav mobile ================================ */}
-                <div className="mobile-menu">
-                    <div className="sidenav-btn">
-                        <div className="hamburger">
-                            <div></div>
-                            <div></div>
-                            <div></div>
-                        </div>
-                    </div>
-                </div>
-                {/* ================================ End nav mobile ================================ */}
-
+        <a href="#" className="logo flex items-center justify-between gap-2">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/f/f9/%E0%B8%95%E0%B8%A3%E0%B8%B2%E0%B8%81%E0%B8%A3%E0%B8%B0%E0%B8%97%E0%B8%A3%E0%B8%A7%E0%B8%87%E0%B8%AA%E0%B8%B2%E0%B8%98%E0%B8%B2%E0%B8%A3%E0%B8%93%E0%B8%AA%E0%B8%B8%E0%B8%82%E0%B9%83%E0%B8%AB%E0%B8%A1%E0%B9%88.png" alt="logo" />
+            <div className="flex flex-col justify-center border">
+                <h2 className="p-0 text-3xl">ศูนย์สุขภาพจิตที่ 9</h2>
+                <p className="p-0 text-sm">Mental Health Center 9</p>
             </div>
-        </div>
+        </a>
     )
 }
--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Link from 'next/link'
 import NavLogo from './nav-logo'
+import NavMenu from "./nav-menu";
 
 export default function Navbar({ screenWidth, ...props }: { screenWidth: number }) {
     const [toggleMenu, setToggleMenu] = useState(false)
@@ -10,10 +11,54 @@ export default function Navbar({ screenWidth, ...props }: { screenWidth: number 
             <div className="container mx-auto">
                 <div className="wrapper w-full">
                     <div className="menu-container w-full">
+                        <div className="container mx-auto">
+                            <div className="relative flex items-end justify-between pt-1">
+                                {/* Brand */}
+                                <NavLogo />
 
-                        {/* Brand */}
-                        <NavLogo />
+                                {/* ================================ Menu Options ================================ */}
+                                {/* <div className="menu-options flex items-center gap-5">
+                                    <div className="options flex items-center space-x-1">
+                                        <p>ขนาดตัวอักษร</p>
+                                        <a href="#" className="hover:bg-gray-300 bg-gray-200 px-2 text-sm rounded-md">ก</a>
+                                        <a href="#" className="hover:bg-gray-300 bg-gray-200 px-2 text-lg rounded-md">ก</a>
+                                        <a href="#" className="hover:bg-gray-300 bg-gray-200 px-2 text-2xl rounded-md">ก</a>
+                                    </div>
+                                    <div className="options flex items-center space-x-1">
+                                        <p>ความตัดกันของสี</p>
+                                        <a href="#" className="hover:bg-gray-300 bg-gray-200 text-sm font-bold px-2 py-1 rounded-md">C</a>
+                                        <a href="#" className="hover:bg-gray-500 bg-gray-600 text-sm font-bold text-white hover:text-gray-100 px-2 py-1 rounded-md">C</a>
+                                        <a href="#" className="hover:bg-gray-700 bg-black text-sm font-bold text-yellow-300 px-2 py-1 rounded-md">C</a>
+                                    </div>
+                                    <div className="options flex items-center space-x-1">
+                                        <div className="border px-2 rounded-lg">
+                                            <select className="outline-none w-full">
+                                                <option value="">ภาษาไทย</option>
+                                                <option value="">English</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div> */}
+                                {/* ================================ End menu Options ================================ */}
 
+                                {/* ================================ Nav menus ================================ */}
+                                <NavMenu />
+                                {/* ================================ End nav menus ================================ */}
+
+                                {/* ================================ Nav mobile ================================ */}
+                                <div className="mobile-menu">
+                                    <div className="sidenav-btn">
+                                        <div className="hamburger">
+                                            <div></div>
+                                            <div></div>
+                                            <div></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                {/* ================================ End nav mobile ================================ */}
+
+                            </div>
+                        </div>
                     </div>
                     {/* /.menu-container */}
                 </div>
