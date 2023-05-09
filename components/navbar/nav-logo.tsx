4c128963c65473a92f94f101e316dab1abfb63e7
sanyath007 <sanyath007@gmail.com>
2023-05-09 11:05:38 +0700

    Edit NavLogo by move logo css class from a tag to picture tag

--- components/navbar/nav-logo.tsx
+++ components/navbar/nav-logo.tsx
@@ -1,8 +1,8 @@
 
 export default function NavLogo() {
     return (
-        <a href="#" className="logo flex items-center justify-between gap-2">
-            <picture>
+        <a href="#" className="flex items-center justify-between gap-2">
+            <picture className="logo">
                 <img
                     src="https://upload.wikimedia.org/wikipedia/commons/f/f9/%E0%B8%95%E0%B8%A3%E0%B8%B2%E0%B8%81%E0%B8%A3%E0%B8%B0%E0%B8%97%E0%B8%A3%E0%B8%A7%E0%B8%87%E0%B8%AA%E0%B8%B2%E0%B8%98%E0%B8%B2%E0%B8%A3%E0%B8%93%E0%B8%AA%E0%B8%B8%E0%B8%82%E0%B9%83%E0%B8%AB%E0%B8%A1%E0%B9%88.png"
                     alt="logo"
