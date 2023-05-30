d4655c623687762b47ca7b4c8464376f02c1f95f
sanyath007 <sanyath007@gmail.com>
2023-05-30 14:21:43 +0700

    Modify Contact component by adding contact detail and re-set embeded google map's size

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,4 +1,6 @@
 import Head from "next/head"
+import Link from 'next/link'
+import { FaFacebook, FaInstagram, FaYoutube, FaTwitter, FaLine, FaMapMarkerAlt } from 'react-icons/fa'
 
 export default function Contact() {
     return (
@@ -16,11 +18,44 @@ export default function Contact() {
 
                     <hr className="my-2" />
 
-                    <div className="contact h-full my-10">
+                    <div className="contact h-full my-4">
                         <div className="container">
-
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d481.77113921239504!2d102.10472111115492!3d14.98331467849054!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31194c9cbc345f15%3A0x9bb83d25272fc25f!2z4Lio4Li54LiZ4Lii4LmM4Liq4Li44LiC4Lig4Liy4Lie4LiI4Li04LiV4LiX4Li14LmIIDk!5e0!3m2!1sth!2sth!4v1677488232052!5m2!1sth!2sth" width="600" height="450" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-
+                            <div className="flex gap-5">
+                                <div className="flex flex-col w-3/4">
+                                    <iframe
+                                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d481.77113921239504!2d102.10472111115492!3d14.98331467849054!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31194c9cbc345f15%3A0x9bb83d25272fc25f!2z4Lio4Li54LiZ4Lii4LmM4Liq4Li44LiC4Lig4Liy4Lie4LiI4Li04LiV4LiX4Li14LmIIDk!5e0!3m2!1sth!2sth!4v1677488232052!5m2!1sth!2sth"
+                                        style={{ width: '100%', height: '520px', border: '1px solid #dadada' }}
+                                        loading="lazy"
+                                        referrerPolicy="no-referrer-when-downgrade"
+                                    ></iframe>
+                                </div>
+                                <div className="flex flex-col w-3/12">
+                                    <h2 className="font-bold text-xl my-2">ศูนย์สุขภาพจิตที่ 9</h2>
+                                    <ul className="text-sm font-light space-y-2">
+                                        <li>เลขที่ 86 ถ.ช้างเผือก อ.เมือง จ.นครราชสีมา 30000</li>
+                                        <li>โทรศัพท์ 0-4425-6729</li>
+                                        <li>โทรสาร 0-4425-6730</li>
+                                        <li>อีเมล mhc9dmh@gmail.com</li>
+                                    </ul>
+                                    <div className="social-icons flex gap-4 mt-4 text-2xl">
+                                        <a href="https://www.facebook.com/mhc9dmh" target="_blank" rel="noreferrer" className="hover:text-gray-300">
+                                            <FaFacebook />
+                                        </a>
+                                        <a href="#" target="_blank" rel="noreferrer" className="hover:text-gray-300">
+                                            <FaTwitter />
+                                        </a>
+                                        <a href="#" target="_blank" rel="noreferrer" className="hover:text-gray-300">
+                                            <FaInstagram />
+                                        </a>
+                                        <a href="https://www.youtube.com/@user-vf2ep3tx6h" target="_blank" rel="noreferrer" className="hover:text-gray-300">
+                                            <FaYoutube />
+                                        </a>
+                                        <a href="#" className="hover:text-gray-300">
+                                            <FaLine size={'22px'} />
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
