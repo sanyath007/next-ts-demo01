2074b83cb1c123d4d4895d3e0cdfecd97241af18
sanyath007 <sanyath007@gmail.com>
2023-08-25 08:01:41 +0700

    Create new MenuOptions and MenuMobile components and add them to Navbar instead of hard coding

---
+++ components/navbar/menu-mobile.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const MenuMobile = () => {
+    return (
+        <div className="mobile-menu">
+            <div className="sidenav-btn">
+                <div className="hamburger">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default MenuMobile
--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import Link from 'next/link'
 import NavLogo from './nav-logo'
 import NavMenu from "./nav-menu";
+import MenuOptions from './menu-options';
+import MenuMobile from './menu-mobile';
 
 export default function Navbar({ screenWidth, ...props }: { screenWidth: number }) {
     const [toggleMenu, setToggleMenu] = useState(false)
@@ -18,28 +20,7 @@ export default function Navbar({ screenWidth, ...props }: { screenWidth: number 
                                 {/* ================================ End Nav Logo ================================ */}
 
                                 {/* ================================ Menu Options ================================ */}
-                                {/* <div className="menu-options flex items-center gap-5">
-                                    <div className="options flex items-center space-x-1">
-                                        <p>ขนาดตัวอักษร</p>
-                                        <a href="#" className="hover:bg-gray-300 bg-gray-200 px-2 text-sm rounded-md">ก</a>
-                                        <a href="#" className="hover:bg-gray-300 bg-gray-200 px-2 text-lg rounded-md">ก</a>
-                                        <a href="#" className="hover:bg-gray-300 bg-gray-200 px-2 text-2xl rounded-md">ก</a>
-                                    </div>
-                                    <div className="options flex items-center space-x-1">
-                                        <p>ความตัดกันของสี</p>
-                                        <a href="#" className="hover:bg-gray-300 bg-gray-200 text-sm font-bold px-2 py-1 rounded-md">C</a>
-                                        <a href="#" className="hover:bg-gray-500 bg-gray-600 text-sm font-bold text-white hover:text-gray-100 px-2 py-1 rounded-md">C</a>
-                                        <a href="#" className="hover:bg-gray-700 bg-black text-sm font-bold text-yellow-300 px-2 py-1 rounded-md">C</a>
-                                    </div>
-                                    <div className="options flex items-center space-x-1">
-                                        <div className="border px-2 rounded-lg">
-                                            <select className="outline-none w-full">
-                                                <option value="">ภาษาไทย</option>
-                                                <option value="">English</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div> */}
+                                <MenuOptions />
                                 {/* ================================ End menu Options ================================ */}
 
                                 {/* ================================ Nav menus ================================ */}
@@ -47,15 +28,7 @@ export default function Navbar({ screenWidth, ...props }: { screenWidth: number 
                                 {/* ================================ End nav menus ================================ */}
 
                                 {/* ================================ Nav mobile ================================ */}
-                                <div className="mobile-menu">
-                                    <div className="sidenav-btn">
-                                        <div className="hamburger">
-                                            <div></div>
-                                            <div></div>
-                                            <div></div>
-                                        </div>
-                                    </div>
-                                </div>
+                                <MenuMobile />
                                 {/* ================================ End nav mobile ================================ */}
                             </div>
                         </div>
